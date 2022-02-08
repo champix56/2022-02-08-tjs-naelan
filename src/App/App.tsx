692b1e59ffb2ed213fa49ac6a3d5860058669813
champix56 <desorbaix@free.fr>
2022-02-08 14:34:15 +0100

    mise en place d'un button simple avec children et fonction

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div className="App">
-        Demat breizh
+        Demat breizh<br/>
+        <Button onButtonClicked={(arg:string)=>{alert('app button clicked' + arg);}}>
+            bla bla  
+            <img src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0/1519855918965?e=2159024400&amp;v=beta&amp;t=CrP5Le1mWICRcaxIGNBuajHcHGFPuyNA5C8DI339lSk" alt="logo"/>
+          </Button>
     </div>
   );
 }
--- src/App/components/Button/Button.tsx
+++ src/App/components/Button/Button.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
-interface I_PropsButton{
+import React from "react";
+interface I_PropsButton {
+  onButtonClicked:Function;
+}
+const Button: React.FC<I_PropsButton> = (props) => {
+  console.log(props);
 
-}
-const Button :React.FC<I_PropsButton>=(props)=>{
-    return <button>Benjamin</button>
-}
+  return (
+    <button
+      onClick={() => {
+        props.onButtonClicked('Le boutton est bien');
+      }}
+    >
+      {/* execution du on click fournit par le parent par les props*/}
+      {props.children}
+      {/* affichage du text fournit par le parent par les props  */}
+    </button>
+  );
+};
 export default Button;

80fcc9882693e6ce07326c031a4f3e018f0e2aa7
champix56 <desorbaix@free.fr>
2022-02-08 15:00:06 +0100

    usage de css module + style dynamique par props

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,7 +6,13 @@ function App() {
   return (
     <div className="App">
         Demat breizh<br/>
-        <Button onButtonClicked={(arg:string)=>{alert('app button clicked' + arg);}}>
+        <Button bgColor="red" onButtonClicked={(arg:string)=>{alert('app button clicked' + arg);}}>
+            bla bla  <br/> node 2
+          </Button>
+          <Button bgColor="blue" onButtonClicked={(arg:string)=>{alert('app button clicked' + arg);}}>
+            bla bla  <br/> node 2
+          </Button>
+          <Button onButtonClicked={(arg:string)=>{alert('app button clicked' + arg);}}>
             bla bla  <br/> node 2
           </Button>
     </div>
--- src/App/components/Button/Button.tsx
+++ src/App/components/Button/Button.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import style from './Button.module.css';
+
+
 interface I_PropsButton {
   onButtonClicked:Function;
   //forcer l'array pour children
   children:Array<any>;
+  bgColor?:string;
 }
 const Button: React.FC<I_PropsButton> = (props) => {
   console.log(props);
 
   return (
-    <button className="Button" style={ {color:'blue'} } 
+    <button className={style.Button} style={{backgroundColor:props.bgColor}}
       onClick={() => {
         props.onButtonClicked('Le boutton est bien');
       }}

4a0e06e63a3cd3b9c0b84c804c0710025b9cab7e
champix56 <desorbaix@free.fr>
2022-02-08 14:44:37 +0100

    changement des enfants

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -7,8 +7,7 @@ function App() {
     <div className="App">
         Demat breizh<br/>
         <Button onButtonClicked={(arg:string)=>{alert('app button clicked' + arg);}}>
-            bla bla  
-            <img src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0/1519855918965?e=2159024400&amp;v=beta&amp;t=CrP5Le1mWICRcaxIGNBuajHcHGFPuyNA5C8DI339lSk" alt="logo"/>
+            bla bla  <br/> node 2
           </Button>
     </div>
   );

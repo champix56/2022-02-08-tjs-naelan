98cc5a7e2aaedd4c96431c035b83f3b7d7bee957
champix56 <desorbaix@free.fr>
2022-02-09 11:54:01 +0100

    correction du test memesvgViewer

--- src/App/components/ui/MemeSvgViewer/MemeSvgViewer.test.tsx
+++ src/App/components/ui/MemeSvgViewer/MemeSvgViewer.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import MemeSvgViewer from './MemeSvgViewer';
-import { images, meme } from '../../../App';
+import { images, initialMeme as meme } from '../../../App';
 
 describe('<MemeSvgViewer />', () => {
   test('it should mount', () => {

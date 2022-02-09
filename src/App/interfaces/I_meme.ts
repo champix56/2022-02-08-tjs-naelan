1e9f50088e8d9658f291cd8d274b9a4c0c039bcc
champix56 <desorbaix@free.fr>
2022-02-09 09:45:27 +0100

    interfaces de meme

---
+++ src/App/interfaces/I_meme.ts
@@ -0,0 +1,18 @@
+export interface I_meme{
+    title:string;
+    text:string;
+    x:number;
+    y:number;
+    color:string;
+    fontSize:number;
+    fontWeight:string;
+    underline:boolean;
+    italic:boolean;
+    imageId:number;
+}
+export interface I_memeImage{
+    title:string;
+    href:string;
+    w:number;
+    h:number;
+}

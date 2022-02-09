b0d1f2917fee94f012c3b0000715f5eca06c3b09
champix56 <desorbaix@free.fr>
2022-02-09 09:50:22 +0100

    ajout des interface de props

--- src/App/components/ui/MemeSvgViewer/MemeSvgViewer.tsx
+++ src/App/components/ui/MemeSvgViewer/MemeSvgViewer.tsx
@@ -1,7 +1,11 @@
 import React, { FC } from 'react';
+import { I_meme, I_memeImage } from '../../../interfaces/I_meme';
 import styles from './MemeSvgViewer.module.css';
 
-interface MemeSvgViewerProps {}
+interface MemeSvgViewerProps {
+  meme:I_meme;
+  image:I_memeImage;
+}
 
 const MemeSvgViewer: FC<MemeSvgViewerProps> = (props) => {
   return(

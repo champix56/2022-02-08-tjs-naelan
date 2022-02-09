b37309d436d76c29a94658368164c4895c56c027
champix56 <desorbaix@free.fr>
2022-02-09 17:14:27 +0100

    fix statics values for test

--- src/App/components/ui/MemeSvgViewer/MemeSvgViewer.test.tsx
+++ src/App/components/ui/MemeSvgViewer/MemeSvgViewer.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import MemeSvgViewer from './MemeSvgViewer';
-import { images, initialMeme as meme } from '../../../App';
-
+import {  initialMeme as meme } from '../../../App';
+import { I_memeImage } from '../../../interfaces/I_meme';
+export const images: Array<I_memeImage> = [
+  { id: 0, title: "futurama all", h: 1315, w: 2160, href: '/img/futurama.png' },
+  { id: 1, title: "futurama solo", h: 1080, w: 1920, href: '/img/futurama2.png' },
+ ];
 describe('<MemeSvgViewer />', () => {
   test('it should mount', () => {
     render(<MemeSvgViewer meme={meme} image={images[0]} />);

a0c7bf97ae349c5117ce89e17d3ddfb75b426530
champix56 <desorbaix@free.fr>
2022-02-09 10:09:01 +0100

    reformat svg

--- src/App/components/ui/MemeSvgViewer/MemeSvgViewer.tsx
+++ src/App/components/ui/MemeSvgViewer/MemeSvgViewer.tsx
@@ -1,20 +1,33 @@
-import React, { FC } from 'react';
-import { I_meme, I_memeImage } from '../../../interfaces/I_meme';
-import styles from './MemeSvgViewer.module.css';
+import React, { FC } from "react";
+import { I_meme, I_memeImage } from "../../../interfaces/I_meme";
+import styles from "./MemeSvgViewer.module.css";
 
 interface MemeSvgViewerProps {
-  meme:I_meme;
-  image:I_memeImage;
+  meme: I_meme;
+  image: I_memeImage;
 }
 
 const MemeSvgViewer: FC<MemeSvgViewerProps> = (props) => {
-  return(
-  <svg className={styles.MemeSvgViewer} data-testid="MemeSvgViewer" width="100%" height="100%">
-    <image href={props.image.href} x="0" y="0"/>
-    <text x={props.meme.x} y={props.meme.y} fontSize={props.meme.fontSize} fontWeight={props.meme.fontWeight} textDecoration={props.meme.underline?'underline':'none'} fontStyle={props.meme.italic?'italic':'normal'}>
+  return (
+    <svg
+      className={styles.MemeSvgViewer}
+      data-testid="MemeSvgViewer"
+      width="100%"
+      height="100%"
+    >
+      <image href={props.image.href} x="0" y="0" />
+      <text
+        x={props.meme.x}
+        y={props.meme.y}
+        fontSize={props.meme.fontSize}
+        fontWeight={props.meme.fontWeight}
+        textDecoration={props.meme.underline ? "underline" : "none"}
+        fontStyle={props.meme.italic ? "italic" : "normal"}
+      >
         {props.meme.text}
-    </text>
-  </svg>
-);}
-MemeSvgViewer.defaultProps={};
+      </text>
+    </svg>
+  );
+};
+MemeSvgViewer.defaultProps = {};
 export default MemeSvgViewer;

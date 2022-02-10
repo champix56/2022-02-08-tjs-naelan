9b95e8e29d34765781cea294303b6a7aeb3c463e
champix56 <desorbaix@free.fr>
2022-02-10 16:42:14 +0100

    mise en place du context router principal

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App/App';
-import { Provider } from 'react-redux';
-import { store } from './App/store/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App/App";
+import { Provider } from "react-redux";
+import { store } from "./App/store/store";
 // import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
